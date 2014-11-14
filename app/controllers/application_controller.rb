ca33e71829b559d1a5fcb09d9e3998ff8660b302
Dare Developers <extreme.daredevelopers@gmail.com>
2014-11-15 00:27:47 +0530

    Student and Lecturer Profile update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def not_authorized
@@ -16,4 +17,21 @@ class ApplicationController < ActionController::Base
       |opts={}| send("current_#{k.underscore}") || not_authorized 
     end 
   end
+
+  protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:first_name, :last_name, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+    }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
+  end
+  
 end
